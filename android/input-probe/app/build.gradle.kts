76547d206febc51f6e2489caadfd356f2312aabd
LightChainr <Sarster@foxmail.com>
2026-07-16 21:41:13 +0800

    Configure input probe Android app

---
+++ android/input-probe/app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "io.github.lightchainr.inkscapeandroid.inputprobe"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "io.github.lightchainr.inkscapeandroid.inputprobe"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
